a6893c857ca2d35fccc0d381fd4306f284403a5a
Giancarlo M. Saverino <giancasaverino@gmail.com>
2021-02-24 09:53:26 +0100

    Everything fixed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   const today = new Date();
-  const year = toString(today.getFullYear);
+  const year = today.getFullYear();
   const text = `Copyright by me: ${year}`; // year OBJECT UNDEFINED???
   return (
     <div className="App">
